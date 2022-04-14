1c736c64d041257d2ebc36a59cb7dcb64ae6403b
Tunaberk Almaci <almacitunaberk@gmail.com>
2022-04-13 23:14:53 -0400

    Initiated server implementation

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,1 +1,42 @@
-package blog_server
+package main
+
+import (
+	"blog_project/blog/blogpb"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+)
+
+type server struct {
+	blogpb.UnimplementedBlogServiceServer
+}
+
+func main() {
+	fmt.Println("Hello")
+	lis, err := net.Listen("tcp", "0.0.0.0:5001")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v\n", err)
+	}
+	opts := []grpc.ServerOption{}
+	s := grpc.NewServer(opts...)
+	blogpb.RegisterBlogServiceServer(s, &server{})
+	go func() {
+		fmt.Println("Starting server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v\n", err)
+		}
+	}()
+	// Wait for Control C to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	// Block until a signal is received
+	<-ch
+	fmt.Println("Stopping the server")
+	s.Stop()
+	fmt.Println("Closing the listener")
+	lis.Close()
+	fmt.Println("End of program")
+}
